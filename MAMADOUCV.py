68156ada42a3715610d77a66f6743f8252987160
fayekholemaurice <fayekholemaurice@gmail.com>
2026-02-27 13:14:57 +0000

    Add files via upload

---
+++ MAMADOUCV.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+with st.sidebar:
+    st.sidebar.header ("mes données personnelles")
+    st.write("Mamadou Diouf")
+    st.write("Email: dioufpapa77841@gmail.com")
+    st.write("Adresse: Gueule tapée")
+
+st.header ("Technicien supérieur en géomatique")
+
+st.subheader ("projet académique")
+st.write ("2023-2024 : BACCALAURIAT S2")
+st.write ("2024-2025 : Première année BTS")
+st.write ("2025-2026 : Deuxième années BTS")
+
+st.subheader ("expériences professionnelles")
+st.write ("participation a un projet de collection avec les étudiants de la géomatique")
+st.write ("2020 à nos jours : Vente de matériel informattique")
+st.write ("21 décembre 2025 jusqu'au 18 janvier 2026 : vente de maillot du Sénégal")
+
+st.subheader("Compétences")
+st.write("Conception de base de données sur QUANTUM GIS et ARCGIS")
+st.write ("Maitrise des matérielles de topograpghie : niveau de chantier , station total , GPS différentiel")
+st.write("Géoreferencement de carte")
+st.write("Numérisation")
+st.write("Maitrise des techniques de collecte données:application Mobile Topographer,locus mapLite")
+st.write("Conception  de Plans sur Autocad")
+
+
+st.subheader ("perspective")
+st.write ("devenir ingénieur en topographie")
